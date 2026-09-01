f5b474fefb4548460988627288fd0a37281009df
ArjunaParth <pandeyparth752@gmail.com>
2026-09-01 20:45:08 +0530

    Delete MAT1008_LAB2_26MIC7101_TASK5.m

--- MAT1008_LAB2_26MIC7101_TASK5.m
+++
@@ -1,9 +0,0 @@
-% 1. Create the vector x and evaluate the polynomial
-x = -2 : 0.25 : 4;
-f = x.^2 - 3*x + 2;
-
-% 2. Extract values where x is 0, 1, or 2
-target_values = f(x == 0 | x == 1 | x == 2);
-
-% 3. Display the result
-disp(target_values)
